5dea4ba9e909785e1b40b28a7ce9f0fa063d839f
Mathieu Wang <mathieu.wang@ericsson.com>
2013-08-09 16:37:22 -0400

    Refactor GerritManagement page.
    The /gerrit-trigger page will now first show the list of servers.
    The configurations for each server can be viewed and changed by
    clicking on the server from this page, which will lead to
    /gerrit-trigger/server/serverName.
    
    Change-Id: I1312130e41dcd7a1de64d5a0aa02a56b4301362e

--- gerrithudsontrigger/src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/GerritManagement.java
+++ gerrithudsontrigger/src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/GerritManagement.java
@@ -24,39 +24,28 @@
  */
 package com.sonyericsson.hudson.plugins.gerrit.trigger;
 
-import com.sonyericsson.hudson.plugins.gerrit.gerritevents.GerritDefaultValues;
-import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshConnectionFactory;
-import com.sonyericsson.hudson.plugins.gerrit.gerritevents.watchdog.WatchTimeExceptionData;
-import com.sonyericsson.hudson.plugins.gerrit.trigger.config.IGerritHudsonTriggerConfig;
 import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.Authentication;
 import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshAuthenticationException;
 import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshConnectException;
 import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshConnection;
+import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshConnectionFactory;
 import com.sonyericsson.hudson.plugins.gerrit.gerritevents.ssh.SshUtil;
+import com.sonyericsson.hudson.plugins.gerrit.trigger.config.IGerritHudsonTriggerConfig;
 import com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.GerritAdministrativeMonitor;
 import hudson.Extension;
 import hudson.model.AdministrativeMonitor;
 import hudson.model.Describable;
-import hudson.model.AbstractProject;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
 import hudson.model.ManagementLink;
 import hudson.model.Saveable;
 import hudson.util.FormValidation;
-import hudson.util.FormValidation.Kind;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.servlet.ServletException;
-
-import jenkins.model.Jenkins;
 
 import net.sf.json.JSONObject;
 
@@ -68,10 +57,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.sonyericsson.hudson.plugins.gerrit.trigger.utils.StringUtil.PLUGIN_IMAGES_URL;
-import static com.sonyericsson.hudson.plugins.gerrit.gerritevents.watchdog.WatchTimeExceptionData.Time.MIN_HOUR;
-import static com.sonyericsson.hudson.plugins.gerrit.gerritevents.watchdog.WatchTimeExceptionData.Time.MAX_HOUR;
-import static com.sonyericsson.hudson.plugins.gerrit.gerritevents.watchdog.WatchTimeExceptionData.Time.MIN_MINUTE;
-import static com.sonyericsson.hudson.plugins.gerrit.gerritevents.watchdog.WatchTimeExceptionData.Time.MAX_MINUTE;
 
 /**
  * Management link for configuring the global configuration of this trigger.
@@ -81,16 +66,8 @@ import static com.sonyericsson.hudson.plugins.gerrit.gerritevents.watchdog.Watch
 public class GerritManagement extends ManagementLink implements StaplerProxy, Describable<GerritManagement>, Saveable {
 
     private static final Logger logger = LoggerFactory.getLogger(GerritManagement.class);
-    private static final String START_SUCCESS = "Connection started";
-    private static final String START_FAILURE = "Error establising conection";
-    private static final String STOP_SUCCESS = "Connection stopped";
-    private static final String STOP_FAILURE = "Error terminating connection";
-    private static final String RESTART_SUCCESS = "Connection restarted";
-    private static final String RESTART_FAILURE = "Error restarting connection";
-    private static final String NEW_SERVER = "New";
 
-    private String selectedServer;
-    private String connectionResponse = "";
+    private static final String NEW_SERVER = "New";
 
     @Override
     public String getIconFileName() {
@@ -115,14 +92,6 @@ public class GerritManagement extends ManagementLink implements StaplerProxy, De
     @Override
     public DescriptorImpl getDescriptor() {
         return Hudson.getInstance().getDescriptorByType(DescriptorImpl.class);
-    }
-
-    /**
-     * Convenience method for Jelly.
-     * @return newServer a String to indicate a new server
-     */
-    public String getNewServer() {
-        return NEW_SERVER;
     }
 
     /**
@@ -198,10 +167,23 @@ public class GerritManagement extends ManagementLink implements StaplerProxy, De
             } else {
                 return FormValidation.error(Messages.BadSshkeyOrPasswordError());
             }
-
-        }
+    }
     }
 
+    /**
+    * Used when redirected to a server.
+    * @param serverName the name of the server.
+    * @return the GerritServer object.
+    */
+    public GerritServer getServer(String serverName) {
+        if (NEW_SERVER.equalsIgnoreCase(serverName)) {
+            logger.info("getting new server");
+            return new GerritServer(NEW_SERVER);
+        } else {
+            logger.info("getting server");
+            return PluginImpl.getInstance().getServer(serverName);
+        }
+    }
     /**
      * Saves the form to the configuration and disk.
      * @param req StaplerRequest
@@ -217,304 +199,14 @@ public class GerritManagement extends ManagementLink implements StaplerProxy, De
             logger.debug("submit {}", req.toString());
         }
         JSONObject form = req.getSubmittedForm();
-        JSONObject dropdown = form.getJSONObject("dropdown");
 
         PluginImpl plugin = PluginImpl.getInstance();
 
-        /*
-         * Multiple dropdownListBlock causes the key to be empty.
-         * This is a known bug as referenced at:
-         *
-         * https://issues.jenkins-ci.org/browse/JENKINS-7517?page=
-         * com.atlassian.jira.plugin.system.issuetabpanels:all-tabpanel
-         *
-         * As such we have to use the empty key and check the
-         * corresponding value to determine which server we are configuring.
-         */
         ArrayList<GerritServer> servers = plugin.getServers();
-        if (form.getString("").equals(NEW_SERVER)) { //"New.." has been selected in the dropdown list
-            String newServerName = dropdown.getString("name");
-            if (!serverListContainsName(newServerName)) {
-                GerritServer server = new GerritServer(newServerName);
-                servers = plugin.addServer(server);
-                selectedServer = newServerName;
-                server.start();
-            }
-        } else { //An existing server has been selected in the dropdown list
-            selectedServer = form.getString("");
-            if (dropdown.getBoolean("removeServer")) { //"remove server" option is checked
-                GerritServer server = plugin.getServer(selectedServer);
-                List<AbstractProject> configuredJobs = server.getConfiguredJobs();
-
-                if (configuredJobs.isEmpty()) { //No job has selected this server, remove safely.
-                    server.stopConnection();
-                    server.stop();
-                    servers = plugin.removeServer(server);
-                } else { //Display error message and the names of the jobs selecting this server.
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("Cannot remove server; configured in the following jobs:\n");
-                    String rootURL = Jenkins.getInstance().getRootUrl();
-                    if (rootURL != null) {
-                        for (AbstractProject job : configuredJobs) {
-                            sb.append("<div>");
-                            sb.append("<a href=");
-                            sb.append(rootURL);
-                            sb.append(job.getUrl());
-                            sb.append("configure>");
-                            sb.append(job.getName());
-                            sb.append("</a>");
-                            sb.append("</div>");
-                        }
-                    } else {
-                        sb.append("(Links disabled because root URL is not set in Manage Jenkins > Configure System)\n");
-                        for (AbstractProject job : configuredJobs) {
-                            sb.append("<div>");
-                            sb.append(job.getName());
-                            sb.append("</div>");
-                        }
-                    }
-                    throw FormValidation.respond(Kind.ERROR, sb.toString());
-                }
-            } else {
-                for (GerritServer s : servers) {
-                    if (s.getName().equals(selectedServer)) {
-                        s.getConfig().setValues(dropdown);
-                    }
-                }
-            }
-        }
-        plugin.setServers(servers);
-        plugin.save();
 
         rsp.sendRedirect(".");
     }
 
-    /**
-     *
-     * @param req the StaplerRequest
-     * @param rsp the StaplerResponse
-     * @throws IOException if unable to send redirect.
-     */
-    public void doConnectionSubmit(StaplerRequest req, StaplerResponse rsp) throws IOException {
-
-        GerritServer server = PluginImpl.getInstance().getServer(selectedServer);
-        setConnectionResponse("");
-        if (req.getParameter("button").equals("Start")) {
-            try {
-                server.startConnection();
-                //TODO wait for the connection to actually be established.
-                //setConnectionResponse(START_SUCCESS);
-            } catch (Exception ex) {
-                setConnectionResponse(START_FAILURE);
-                logger.error("Could not start connection. ", ex);
-            }
-        } else if (req.getParameter("button").equals("Stop")) {
-            try {
-                server.stopConnection();
-                //TODO wait for the connection to actually be shutdown.
-                //setConnectionResponse(STOP_SUCCESS);
-            } catch (Exception ex) {
-                setConnectionResponse(STOP_FAILURE);
-                logger.error("Could not stop connection. ", ex);
-            }
-        } else {
-            try {
-                server.restartConnection();
-                //TODO wait for the connection to actually be shut down and connected again.
-                //setConnectionResponse(RESTART_SUCCESS);
-            } catch (Exception ex) {
-                setConnectionResponse(RESTART_FAILURE);
-                logger.error("Could not restart connection. ", ex);
-            }
-        }
-        rsp.sendRedirect(".");
-    }
-
-    /**
-     * Get the response after a start/stop/restartConnection; Used by jelly.
-     * @return the connection response
-     */
-    public String getConnectionResponse() {
-        return connectionResponse;
-    }
-
-    /**
-     * Set the connection status.
-     * @param response the response to be set.
-     */
-    private void setConnectionResponse(String response) {
-        connectionResponse = response;
-    }
-
-    /**
-     * Checks that the provided parameter is an integer and not negative.
-     * @param value the value.
-     * @return {@link FormValidation#validatePositiveInteger(String)}
-     */
-    public FormValidation doPositiveIntegerCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        return FormValidation.validatePositiveInteger(value);
-    }
-
-    /**
-     * Checks that the provided parameter is an integer and not negative, zero is accepted.
-     *
-     * @param value the value.
-     * @return {@link FormValidation#validateNonNegativeInteger(String)}
-     */
-    public FormValidation doNonNegativeIntegerCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        return FormValidation.validateNonNegativeInteger(value);
-    }
-
-    /**
-     * Checks that the provided parameter is an integer, not negative, that is larger
-     * than the minimum value.
-     * @param value the value.
-     * @return {@link FormValidation#validatePositiveInteger(String)}
-     */
-    public FormValidation doDynamicConfigRefreshCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        FormValidation validatePositive = FormValidation.validatePositiveInteger(value);
-        if (!validatePositive.kind.equals(FormValidation.Kind.OK)) {
-            return validatePositive;
-        } else {
-            int intValue = Integer.parseInt(value);
-            if (intValue < GerritDefaultValues.MINIMUM_DYNAMIC_CONFIG_REFRESH_INTERVAL) {
-                return FormValidation.error(Messages.DynamicConfRefreshTooLowError(
-                        GerritDefaultValues.MINIMUM_DYNAMIC_CONFIG_REFRESH_INTERVAL));
-            }
-        }
-        return FormValidation.ok();
-    }
-
-    /**
-     * Checks that the provided parameter is an integer.
-     * @param value the value.
-     * @return {@link FormValidation#validatePositiveInteger(String)}
-     */
-    public FormValidation doIntegerCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        try {
-            Integer.parseInt(value);
-            return FormValidation.ok();
-        } catch (NumberFormatException e) {
-            return FormValidation.error(hudson.model.Messages.Hudson_NotANumber());
-        }
-    }
-
-    /**
-     * Checks that the provided parameter is an empty string or an integer.
-     * @param value the value.
-     * @return {@link FormValidation#validatePositiveInteger(String)}
-     */
-    public FormValidation doEmptyOrIntegerCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        if (value == null || value.length() <= 0) {
-            return FormValidation.ok();
-        } else {
-            try {
-                Integer.parseInt(value);
-                return FormValidation.ok();
-            } catch (NumberFormatException e) {
-                return FormValidation.error(hudson.model.Messages.Hudson_NotANumber());
-            }
-        }
-    }
-
-    /**
-     * Checks if the value is a valid URL. It does not check if the URL is reachable.
-     * @param value the value
-     * @return {@link FormValidation#ok() } if it is so.
-     */
-    public FormValidation doUrlCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        if (value == null || value.length() <= 0) {
-            return FormValidation.error(Messages.EmptyError());
-        } else {
-            try {
-                new URL(value);
-
-                return FormValidation.ok();
-            } catch (MalformedURLException ex) {
-                return FormValidation.error(Messages.BadUrlError());
-            }
-        }
-    }
-
-    /**
-     * Checks to see if the provided value is a file path to a valid private key file.
-     * @param value the value.
-     * @return {@link FormValidation#ok() } if it is so.
-     */
-    public FormValidation doValidKeyFileCheck(
-            @QueryParameter("value")
-            final String value) {
-
-        File f = new File(value);
-        if (!f.exists()) {
-            return FormValidation.error(Messages.FileNotFoundError(value));
-        } else if (!f.isFile()) {
-            return FormValidation.error(Messages.NotFileError(value));
-        } else {
-            if (SshUtil.isPrivateKeyFileValid(f)) {
-                return FormValidation.ok();
-            } else {
-                return FormValidation.error(Messages.InvalidKeyFileError(value));
-            }
-        }
-    }
-
-    /**
-     * Checks to see if the provided value represents a time on the hh:mm format.
-     * Also checks that from is before to.
-     *
-     * @param fromValue the from value.
-     * @param toValue the to value.
-     * @return {@link FormValidation#ok() } if it is so.
-     */
-    public FormValidation doValidTimeCheck(
-            @QueryParameter final String fromValue, @QueryParameter final String toValue) {
-        String[] splitFrom = fromValue.split(":");
-        String[] splitTo = toValue.split(":");
-        int fromHour;
-        int fromMinute;
-        int toHour;
-        int toMinute;
-        if (splitFrom.length != 2 || splitTo.length != 2) {
-            return FormValidation.error(Messages.InvalidTimeString());
-        }
-        try {
-            fromHour = Integer.parseInt(splitFrom[0]);
-            fromMinute = Integer.parseInt(splitFrom[1]);
-            toHour = Integer.parseInt(splitTo[0]);
-            toMinute = Integer.parseInt(splitTo[1]);
-
-        } catch (NumberFormatException nfe) {
-            return FormValidation.error(Messages.InvalidTimeString());
-        }
-
-        if (fromHour < MIN_HOUR || fromHour > MAX_HOUR || fromMinute < MIN_MINUTE || fromMinute > MAX_MINUTE
-                || toHour < MIN_HOUR || toHour > MAX_HOUR || toMinute < MIN_MINUTE || toMinute > MAX_MINUTE) {
-                return FormValidation.error(Messages.InvalidTimeString());
-        }
-        if (fromHour > toHour || (fromHour == toHour && fromMinute > toMinute)) {
-            return FormValidation.error(Messages.InvalidTimeSpan());
-        }
-        return FormValidation.ok();
-    }
 
     /**
      * Checks whether server name already exists.
@@ -529,16 +221,6 @@ public class GerritManagement extends ManagementLink implements StaplerProxy, De
         } else {
             return FormValidation.ok();
         }
-    }
-
-    /**
-     * Checks whether a server other than "New" is chosen in the dropdown.
-     * Used by jelly to hide control box when adding new server.
-     * @return whether a valid server is chosen.
-     */
-    public boolean isValidServerChosen() {
-        //TODO: get the value of "selectedServer" from the dropdown json object to actually make this method work.
-        return !selectedServer.equals(NEW_SERVER);
     }
 
     @Override
@@ -616,39 +298,4 @@ public class GerritManagement extends ManagementLink implements StaplerProxy, De
         return contains;
     }
 
-    /**
-     * Get the selected server.
-     *
-     * @return the selected server as a String.
-     */
-    public String getSelectedServer() {
-        if (selectedServer == null) {
-            ArrayList<GerritServer> servers = PluginImpl.getInstance().getServers();
-            if (servers.isEmpty()) { //no server previously configured
-                selectedServer = NEW_SERVER;
-            } else { //at least one server previously configured, return the first one.
-                selectedServer = servers.get(0).getName();
-            }
-        }
-        return selectedServer;
-    }
-
-    /**
-     * Generates a list of helper objects for the jelly view.
-     *
-     * @param serverName the name of the server for which we want to generate helper objects.
-     * @return a list of helper objects.
-     */
-    public List<ExceptionDataHelper> generateHelper(String serverName) {
-        WatchTimeExceptionData data = getConfig(serverName).getExceptionData();
-        List<ExceptionDataHelper> list = new LinkedList<ExceptionDataHelper>();
-        list.add(new ExceptionDataHelper(Messages.MondayDisplayName(), Calendar.MONDAY, data));
-        list.add(new ExceptionDataHelper(Messages.TuesdayDisplayName(), Calendar.TUESDAY, data));
-        list.add(new ExceptionDataHelper(Messages.WednesdayDisplayName(), Calendar.WEDNESDAY, data));
-        list.add(new ExceptionDataHelper(Messages.ThursdayDisplayName(), Calendar.THURSDAY, data));
-        list.add(new ExceptionDataHelper(Messages.FridayDisplayName(), Calendar.FRIDAY, data));
-        list.add(new ExceptionDataHelper(Messages.SaturdayDisplayName(), Calendar.SATURDAY, data));
-        list.add(new ExceptionDataHelper(Messages.SundayDisplayName(), Calendar.SUNDAY, data));
-        return list;
-    }
 }
